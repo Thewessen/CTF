4965c45932e648c20e7cd1318ea1ffd8dd670a88
Thewessen <sthewessen@gmail.com>
2019-03-21 09:31:32 +0100

    Progressed level 25, 26 and 27

--- OverTheWire/bandit/login.sh
+++ OverTheWire/bandit/login.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-sshpass -p uNG9O58gUE7snukf3bvZ0rxhtnjzSGzG \
-  ssh -l bandit25 -p 2220 bandit.labs.overthewire.org
+sshpass -p 0ef186ac70e04ea33b4c1853d2526fa2 \
+  ssh -l bandit28 -p 2220 bandit.labs.overthewire.org
   # -t "cd /tmp/tmp.sOrQHW1XdC; bash -l"
 
 # level 14, 17
-# ssh -i level26.private -l bandit26 -p 2220 bandit.labs.overthewire.org
+# ssh -i level26.private -l bandit26 -p 2220 bandit.labs.overthewire.org -t "bash -l"
 
 
