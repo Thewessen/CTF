87c11e1076d633ec5ea0d372ce3d08a1a44c9966
Thewessen <sthewessen@gmail.com>
2019-03-23 11:48:13 +0100

    Minor changes

--- OverTheWire/bandit/login.sh
+++ OverTheWire/bandit/login.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 sshpass -p 0ef186ac70e04ea33b4c1853d2526fa2 \
-  ssh -l bandit28 -p 2220 bandit.labs.overthewire.org
-  # -t "cd /tmp/tmp.sOrQHW1XdC; bash -l"
+  ssh -l bandit28 -p 2220 bandit.labs.overthewire.org \
+  -t "cd /tmp/tmp.sOrQHW1XdC; bash -l"
 
 # level 14, 17
 # ssh -i level26.private -l bandit26 -p 2220 bandit.labs.overthewire.org -t "bash -l"
