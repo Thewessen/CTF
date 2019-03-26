93ca86cbc240ee879f621facec995196c3bfe969
Thewessen <sthewessen@gmail.com>
2019-03-26 23:15:25 +0100

    two more levels...

--- OverTheWire/bandit/login.sh
+++ OverTheWire/bandit/login.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-sshpass -p 5b90576bedb2cc04c86a9e924ce42faf \
-  ssh -l bandit30 -p 2220 bandit.labs.overthewire.org \
+sshpass -p 56a9bf19c63d650ce78e6ec0354ee45e \
+  ssh -l bandit32 -p 2220 bandit.labs.overthewire.org \
   -t "cd /tmp/tmp.sOrQHW1XdC; bash -l"
 
 # level 14, 17
