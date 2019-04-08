d5e7cbbe66dfc367ef354d05f1d2bc76578d1b43
Thewessen <sthewessen@gmail.com>
2019-04-08 08:14:38 +0200

    Created some queries for natas 28

--- OverTheWire/natas/test.php
+++ OverTheWire/natas/test.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
   
   /* $array = array("data" => "¿", "number" => "¿"); */
@@ -6,9 +7,9 @@
 
   /* echo htmlentities("$myarray"); */
   
-  $real_query = "SELECT * from where =a";
-  $query = "G%2BglEae6W%2F1XjA7vRm21nNyEco%2Fc%2BJ2TdR0Qp8dcjPKriAqPE2%2B%2BuYlniRMkobB1vfoQVOxoUVz5bypVRFkZR5BPSyq%2FLC12hqpypTFRyXA%3D";
+  $real_query = array( 'query' => 'a' );
+  $query = "ClVLIh4ASCsCBE8lAxMacFMZV2hdVVotEhhUJQNVAmhSEV4sFxFeaAw%3D";
 
-  print base64_encode($query);
+  echo base64_encode(json_encode($real_query));
 
 ?>
--- OverTheWire/natas/test.py
+++ OverTheWire/natas/test.py
@@ -3,23 +3,34 @@
 import requests, string
 from requests.auth import HTTPBasicAuth
 
-uri = 'http://natas28.natas.labs.overthewire.org/'
+uri = 'http://natas28.natas.labs.overthewire.org/index.php'
 username = 'natas28'
 password = 'JWwR438wkgTsNKBbcJoowyysdM82YjeF'
 
-query = {'query' :
-        'd"'}
+# chars = ''.join([string.digits, string.ascii_letters])
 
-# Create a username natas28 with 64 trailing spaces, the 1 gets trunked and the
-# spaces trimed
-Data = {'username' : "natas28" + " "*64 + "1",'password' : "1"}
+Data = {'query' : 'b',
+        'user' : 'admin'}
 
-chars = ''.join([string.digits, string.ascii_letters])
-
-Data = {'query' : "b"}
-
+params = '&'.join(['%s=%s' % (k, v) for k, v in Data.items()])
+print(params)
+# for i in chars:
+# query = {'query' : i}
 p = requests.get( uri,
-        auth=HTTPBasicAuth(username, password),
-        # data = Data)
-        params = query)
+    auth=HTTPBasicAuth(username, password),
+    # data = Data)
+    params = params)
 print( '\n'.join( p.text.split('\r\n')[24:]) + '\n\n' )
+# print( p.url.split('=')[1] )
+
+# f = open('./query.txt')
+# lines = [line.rstrip('\n') for line in f]
+# query = [k.split('\t') for k in lines]
+# queries = [''.join([v[2],v[3],v[4]]) for v in query]
+# f.close()
+
+# for q in queries:
+#     # g = q + ''
+#     # str(q).encode('base64')
+#     print( str(q[4:-3]).decode('base64') )
+
