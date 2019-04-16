2967f95df2e7606c96af8aee484ffd8e04bc6d1f
Thewessen <sthewessen@gmail.com>
2019-04-16 17:32:58 +0200

    Completed some more krypton levels

--- OverTheWire/krypton/login.sh
+++ OverTheWire/krypton/login.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-sshpass -p ahy7MaeBo9\
-  ssh -l leviathan7 -p 2223 leviathan.labs.overthewire.org
+sshpass -p CAESARISEASY\
+  ssh -l krypton3 -p 2222 krypton.labs.overthewire.org
   # -t "cd /tmp/tmp.sOrQHW1XdC; bash -l"
 
