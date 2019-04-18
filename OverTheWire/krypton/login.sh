2602afbfa42e27aeca45d886f8e952a2bda54a09
Thewessen <sthewessen@gmail.com>
2019-04-19 01:24:22 +0200

    Solved level04 (with python!)

--- OverTheWire/krypton/login.sh
+++ OverTheWire/krypton/login.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-sshpass -p BRUTE\
-  ssh -l krypton4 -p 2222 krypton.labs.overthewire.org
+sshpass -p CLEARTEXT\
+  ssh -l krypton5 -p 2222 krypton.labs.overthewire.org
   # -t "cd /tmp/tmp.sOrQHW1XdC; bash -l"
 
