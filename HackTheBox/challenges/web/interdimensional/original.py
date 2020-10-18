27be6e4af9eaf8a27a3bcfb848726cc525d89e49
Thewessen <sthewessen@gmail.com>
2020-10-18 14:43:09 +0200

    Added the original debug.py from the website

--- HackTheBox/challenges/web/interdimensional/original.py
+++ HackTheBox/challenges/web/interdimensional/original.py
@@ -8,34 +8,26 @@ def calc(recipe):
     global garage
     builtins, garage = {'__builtins__': None}, {}
     try: exec(recipe, builtins, garage)
-    except Exception as e: print(e.args)
+    except: pass
 
 def GFW(func): # Great Firewall of the observable universe and it's infinite timelines
     @functools.wraps(func)
     def federation(*args, **kwargs):
-        session['ingredient'] = 'import os\n\n' + 'a' * 20
-        session['measurements'] = 'os.environ'
         ingredient = session.get('ingredient', None)
         measurements = session.get('measurements', None)
 
         recipe = '%s = %s' % (ingredient, measurements)
-        print(recipe)
         if ingredient and measurements and len(recipe) >= 20:
-            print('INSIDE')
             regex = re.compile('|'.join(map(re.escape, ['[', '(', '_', '.'])))
             matches = regex.findall(recipe)
             
             if matches: 
-                print('Invalid token: ' + ', '.join(set(matches)))
                 return render_template('index.html', blacklisted='Morty you dumbass: ' + ', '.join(set(matches)))
             
             if len(recipe) > 300: 
-                print('Size to big: (%s) %s' % (len(recipe), recipe))
                 return func(*args, **kwargs) # ionic defibulizer can't handle more bytes than that
             
-            print('all checks passed')
             calc(recipe)
-            print(garage)
             # return render_template('index.html', calculations=garage[ingredient])
             return func(*args, **kwargs) # rick deterrent
 
@@ -43,7 +35,6 @@ def GFW(func): # Great Firewall of the observable universe and it's infinite tim
         measurements = session['measurements'] = ''.join(map(str, [random.randint(1, 69), random.choice(['+', '-', '*']), random.randint(1,69)]))
 
         calc('%s = %s' % (ingredient, measurements))
-        print(garage)
         return render_template('index.html', calculations=garage[ingredient])
     return federation
 
