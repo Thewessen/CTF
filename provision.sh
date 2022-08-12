8f33d42bacbfaa9313b6207882619473bbfcf152
Thewessen <sthewessen@gmail.com>
2022-08-12 07:31:43 +0200

    Update provision script (comment out outdated code)

--- provision.sh
+++ provision.sh
@@ -5,6 +5,9 @@ main () {
     # make zsh the default shell
     usermod --shell /bin/zsh vagrant
 
+    # removes warning 'unable to open stdin'
+    export DEBIAN_FRONTEND=noninteractive
+
     # install most wanted packages and config
     upgrade
     add_configs
@@ -14,7 +17,7 @@ main () {
     install_rust
     install_bat
     install_lsd
-    install_python_modules
+    # install_python_modules
     install_seclists
     install_openvpn
     user_accessible
@@ -31,9 +34,11 @@ upgrade () {
     usermod -d /run/ircd irc
 
     # upgrade postgresql manually
-    upgrade_postgresql
+    #FIXME: upgrading postgresql shows popup
+    # upgrade_postgresql
 
     echo "update & upgrade all installed packages"
+    apt-get update
     apt-get -y --no-show-upgraded \
         --asume-yes \
         --allow-change-held-packages \
@@ -41,7 +46,8 @@ upgrade () {
         full-upgrade
 
     echo "update searchsploit manually"
-    searchsploit --update
+    #FIXME: hangs (prob uses apt iso apt-get)
+    # searchsploit --update
 }
 
 upgrade_postgresql () {
@@ -65,10 +71,10 @@ add_configs () {
     git clone -b minimal --single-branch --bare https://github.com/Thewessen/dotfiles.git $homedir/dotfiles
     git --git-dir=$homedir/dotfiles --work-tree=$homedir checkout
     # TODO: remove traces of dotfiles
-    git init --separate-git-dir=$homedir/dotfiles $homedir
+    # git init --separate-git-dir=$homedir/dotfiles $homedir
     cd $homedir
-    git config status.showUntrackedFiles no
-    cd -
+    # git config status.showUntrackedFiles no
+    # cd -
 }
 
 add_zsh_theme () {
@@ -86,10 +92,10 @@ install_fzf () {
 
 install_neovim () {
     echo "build neovim from source"
-    apt-get install --fix-missing -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip python2 python3-pip nodejs npm
+    apt-get install --fix-missing -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
     git clone https://github.com/neovim/neovim.git neovim
     cd neovim
-    make CMAKE_BUILD_TYPE=RelWithDebInfo #this also symlinks bin..
+    make CMAKE_BUILD_TYPE=Release
     sudo make install
     cd -
     rm -rf neovim
@@ -97,7 +103,7 @@ install_neovim () {
     # install neovim providers
     gem install neovim
     npm i -g neovim
-    python2.7 -m pip install neovim
+    # python2.7 -m pip install neovim
     python3 -m pip install neovim
 
 
@@ -122,37 +128,38 @@ install_lsd () {
     cargo install lsd
 }
 
-install_python_modules () {
-    echo "install apt dependencies"
-    curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
-    python2 get-pip.py
-    rm get-pip.py
-    python2 -m pip install wheel
-    python2 -m pip install virtualenv
-}
+# install_python_modules () {
+#     #DECPRECATED
+#     echo "install apt dependencies"
+#     curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+#     python2 get-pip.py
+#     rm get-pip.py
+#     python2 -m pip install wheel
+#     python2 -m pip install virtualenv
+# }
 
 install_seclists () {
     echo "install common wordlists"
-    apt install -y seclists --fix-missing
+    apt-get install -y seclists --fix-missing
 }
 
 install_openvpn () {
     echo "install openvpn"
-    apt install -y openvpn --fix-missing
+    apt-get install -y openvpn --fix-missing
 }
 
 user_accessible () {
     echo "make installed packages accesible by vagrant"
-    mv -r /root/.cargo $homedir/
-    mv -r /root/.rustup $homedir/
+    mv /root/.cargo $homedir/
+    mv /root/.rustup $homedir/
     chown -R vagrant:vagrant $homedir
 }
 
 cleanup () {
     echo "cleanup..."
-    apt-get autoremove -y
+    apt-get autoremove -y --purge
     apt-get clean -y
-    #XXX: rm -rf $homedir/dotfiles
+    rm -rf $homedir/dotfiles
 }
 
 main "$@"
